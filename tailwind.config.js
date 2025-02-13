dd2d745cd2c49dc05b7f82967b5a9e7e32a6e018
Iakovos Kareglis <JacobK7376@gmail.com>
2025-02-13 22:23:40 +0200

    Work Responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",  // Explicitly include index.html
+    "./js/**/*.{js,ts,jsx,tsx}",  // Include JavaScript files inside the js folder
+    "./assets/**/*.{html,js,ts,jsx,tsx}", // Include any relevant files in assets
+  ],
   theme: {
+    screens: {
+      'mobile': '300px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+    },
     extend: {},
   },
   plugins: [],
